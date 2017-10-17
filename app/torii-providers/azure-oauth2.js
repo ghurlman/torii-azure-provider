b05fa87d3d74c1116b50f94069befec687090768
Yoran Brondsema <yoran.brondsema@gmail.com>
2017-10-17 19:41:34 +0200

    Change the URL (#3)
    Microsoft updated their authentication domain to login.microsoftonline.com (see the blog post and current documentation). This makes that change.

--- app/torii-providers/azure-oauth2.js
+++ app/torii-providers/azure-oauth2.js
@@ -1,33 +1,29 @@
 import Oauth2 from 'torii/providers/oauth2-code';
-import {configurable} from 'torii/configuration';
+import { configurable } from 'torii/configuration';
 
 /**
- * This class implements authentication against Azure
- * using the OAuth2 authorization flow in a popup window.
+ * This class implements authentication against Azure using the OAuth2
+ * authorization flow in a popup window.
  * @class
  */
-var AzureOauth2 = Oauth2.extend({
-  name:       'azure-oauth2',
-  baseUrl:    'https://login.windows.net/common/oauth2/authorize',
+export default Oauth2.extend({
+  name: 'azure-oauth2',
+  baseUrl: 'https://login.microsoftonline.com/common/oauth2/authorize',
 
   // additional url params that this provider requires
   requiredUrlParams: ['state'],
 
   responseParams: ['code','session_state','state'],
 
-  state: configurable('state', function(){
-    // A hack that allows redirectUri to be configurable
-    // but default to the superclass
-    return 'STATE';
+  state: configurable('state', function() {
+    // A hack that allows redirectUri to be configurable but default to the
+    // superclass
+    return this._super();
   }),
 
-  response_type: "code",
-
-  redirectUri: configurable('redirectUri', function(){
-    // A hack that allows redirectUri to be configurable
-    // but default to the superclass
+  redirectUri: configurable('redirectUri', function() {
+    // A hack that allows redirectUri to be configurable but default to the
+    // superclass
     return this._super();
   })
 });
-
-export default AzureOauth2;
